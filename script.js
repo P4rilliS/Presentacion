333563cb3c06f6ef6b3e1dda1ae8089174869dc8
Sergio <sergioparilli06@gmail.com>
2024-12-06 06:26:24 -0400

    act

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function cambiarPalabra() {
 }
 
 setInterval(cambiarPalabra, 1000);
+
+
+// script.js 
+function toggleMenu() { 
+    const seleccion = document.getElementById('menu-invisible'); 
+    const iniciar_sesion = document.getElementById('btn-iniciar-sesion'); 
+    if (seleccion.style.display === 'block') { 
+        seleccion.style.display = 'none';
+        iniciar_sesion.style.display = 'none';
+    } else { 
+        seleccion.style.display = 'block';
+        iniciar_sesion.style.display = 'block'; 
+    }
+}
